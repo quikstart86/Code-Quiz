8c664a7c4bfe4c1ca440e6115e990bfd39bf9d9c
Thomas <123500706+quikstart86@users.noreply.github.com>
2023-12-21 17:39:50 +0000

    created a repo and cloned to local/ added html/css/js files and folders to the project/started to sudo code and added in an array for to store the questions and answers

---
+++ script.js
@@ -0,0 +1,41 @@
+// Home page > Welcome message 
+// Start button > need to get access to it (query selector //Get byID)
+// create even listener and attach to start button on click
+
+// When click start > hide welcome message and show quiz area
+// Create class in JS for hide/remove .remove
+
+// Questions array
+let questions = [
+    {
+        question: "",
+        choices: [`A`, `B`, `C`, `D`],
+        Answer: `A`
+    },
+    {
+        question: "",
+        choices: [`A`, `B`, `C`, `D`],
+        Answer: `A`
+    },
+    {
+        question: "",
+        choices: [`A`, `B`, `C`, `D`],
+        Answer: `A`
+    },
+    {
+        question: "",
+        choices: [`A`, `B`, `C`, `D`],
+        Answer: `A`
+    },
+    {
+        question: "",
+        choices: [`A`, `B`, `C`, `D`],
+        Answer: `A`
+    },
+    {
+        question: "",
+        choices: [`A`, `B`, `C`, `D`],
+        Answer: `A`
+    },
+]
+
