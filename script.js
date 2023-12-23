a5d6eb246ab65f6fed175051fa29680774adc615
Thomas <123500706+quikstart86@users.noreply.github.com>
2023-12-23 11:07:52 +0000

    addded in countown function/moved sudo code to html file to make space for js/added some more variables

--- script.js
+++ script.js
@@ -1,107 +1,104 @@
-// Set of questions --> array of objects
-// Each question needs the following:
-  // Question text
-  // Set of answers
-  // Which answer is correct
 
-// Landing page:
-  // Explanation of the quiz
-  // Start button
-
-// Click the start button:
-  // Landing page goes away
-  // Timer starts
-  // The first question appears (with its answers)
-
-// For each question:
-  // User clicks an answer
-  // Their choice is compared to the correct answer as stored in the question's object
-  // If correct, tell them
-  // If incorrect, tell them AND subtract time from the timer
-  // Optional: play a sound for correct or incorrect
-  // Either way, the question disappears after a few seconds and the next question appears
-
-// After the last question:
-  // Timer stops
-  // Question disappears
-  // Form appears for user to enter their initials
-  // Display their score
-
-// User submits form
-  // Initials and score get stored in local storage
-  // User is taken to the high scores page
-  // High scores are listed, sorted highest to lowest
-  // User has option to take the quiz again
-  
-  // Home page > Welcome message 
-// Start button > need to get access to it (query selector //Get byID)
-// create even listener and attach to start button on click
-
-// When click start > hide welcome message and show quiz area
-// Create class in JS for hide/remove .remove
-
-// Questions/Choices/Answers stored as an array
-
+// Sudo code sitting in HTML to take up less window space on JS file
 // <--------------- START OF CODE -----------------> //
+
+
 
 const startScreenPage = document.querySelector("#start-screen");
 const startBtn = document.querySelector("#start");
 
-// ---------Set Questions Array ----------
-let questions = [
-    {
-        question: "What country has the highest life expectancy?",
-        choices: [`Japan`, `Hong Kong`, `Sweden`, `Paraguay`],
-        answer: `Hong Kong`,
-        
-    },
-    {
-        question: "Who was the Ancient Greek God of the Sun?",
-        choices: [`Apollo`, ``, ``, ``],
-        answer: `Apollo`
-    },
-    {
-        question: "How many minutes are in a full week?",
-        choices: [`A`, `B`, `C`, `D. 10,080`],
-        answer: `10,080`
-    },
-    {
-        question: "How many elements are in the periodic table?",
-        choices: [`A`, `B`, `C. 118`, `D`],
-        answer: `118`
-    },
-    {
-        question: "What country drinks the most coffee per capita?",
-        choices: [`A. Finland`, `B`, `C`, `D`],
-        answer: `Finland`
-    },
-    {
-        question: "Which planet in the Milky Way is the hottest?",
-        choices: [`A`, `B`, `C. Venus`, `D`],
-        answer: `Venus`
-    },
-    {
-        question: `What company was initially known as "Blue Ribbon Sports"?`,
-        choices: [`A`, `B`, ``, `D`],
-        answer: `Nike`
-    },
-    {
-        question: "What is the world's fastest bird?",
-        choices: [`A`, `B. The Peregrine Falcon`, `C`, `D`],
-        answer: `The Peregrine Falcon`
-    },
+
+
+// ---------Set Questions Array (10 questions)----------
+const questions = [
+  {
+    question: "What country has the highest life expectancy?",
+    choices: [`Japan`, `Hong Kong`, `Sweden`, `Paraguay`],
+    answer: `Hong Kong`,
+
+  },
+  {
+    question: "Who was the Ancient Greek God of the Sun?",
+    choices: [`Apollo`, ``, ``, ``],
+    answer: `Apollo`
+  },
+  {
+    question: "How many minutes are in a full week?",
+    choices: [`A`, `B`, `C`, `D. 10,080`],
+    answer: `10,080`
+  },
+  {
+    question: "How many elements are in the periodic table?",
+    choices: [`A`, `B`, `C. 118`, `D`],
+    answer: `118`
+  },
+  {
+    question: "What country drinks the most coffee per capita?",
+    choices: [`A. Finland`, `B`, `C`, `D`],
+    answer: `Finland`
+  },
+  {
+    question: "Which planet in the Milky Way is the hottest?",
+    choices: [`A`, `B`, `C. Venus`, `D`],
+    answer: `Venus`
+  },
+  {
+    question: `What company was initially known as "Blue Ribbon Sports"?`,
+    choices: [`A`, `B`, ``, `D`],
+    answer: `Nike`
+  },
+  {
+    question: `What is the world's fastest bird?`,
+    choices: [`something`, `The Peregrine Falcon`, `something`, `something`],
+    answer: `The Peregrine Falcon`
+  },
+  {
+    question: "How tall is the Eiffel Tower in Paris?",
+    choices: [`something`, `something`, `something`, `something`],
+    answer: ``
+  },
+  {
+    question: "What is the collective noun for a group of flamingos?",
+    choices: [`something`, `something`, `something`, `something`],
+    answer: `A Flamboyance`
+  },
 ]
+console.log(questions);
 
 // Setting other variables
 const timeLeft = document.getElementById(`time`);
 const secondsLeft = 60;
 const questionNumber = 0;
-const maxQuestions = 8;
+const maxQuestions = 10;
 const totalScore = 0;
+const questionCount = 1;
+
+
 
 // Function - Time left
 function countdown() {
+let timerInterval = setInterval(function() {
+  secondsLeft--;
+  timeLeft.textContent = `${timeLeft, secondsLeft}`;
 
+  if (secondsLeft <=0){
+    timeLeft.textContent = `Time is up!`;
+
+    // If the timer is up, show timer is up
+    finish.textContent = `Time is up!`;
+    gameOver();
+
+  } else if (questionCount >= questions.length +1) {
+    clearInterval(timerInterval);
+    gameOver();
+  }
+}, 1000);
 }
 
-
+// click the start button to start the quiz
+startQuizBtn.addEventListener(`click`, newQuiz);
+function startQuiz () {
+  questionNumber = 0
+  countdown();
+  showQuestion(questionNumber);
+}
